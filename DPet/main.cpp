6fdc8b656e9b2b920398cba7ff92b5087f41656d
Snowdrop <snowdrop27515@gmail.com>
2026-04-26 20:29:07 +0900

    file upload

---
+++ DPet/main.cpp
@@ -0,0 +1,17 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include "GLFW/glfw3native.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "windows.h"
+#include "shellapi.h"
+
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "opengl32.lib")
+
+int main() {
+	return 0;
+}
