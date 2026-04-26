c8d7992da20124336f906b5e2151d3a373cc201b
Snowdrop <snowdrop27515@gmail.com>
2026-04-27 08:49:49 +0900

    hmm.. ok?

--- DPet/main.cpp
+++ DPet/main.cpp
@@ -24,16 +24,66 @@ public:
 	struct Frame {
 
 	}; static Frame* frame;
+	unsigned int LoadPNG(const char* path) {
+		unsigned int texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		stbi_set_flip_vertically_on_load(true);
+
+		int width, height, channels;
+		unsigned char* data = stbi_load(path, &width, &height, &channels, 4);
+
+		if (data) {
+			GLenum format = (channels == 4) ? GL_RGBA : GL_RGB;
+			glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else {
+			return 0;
+		}
+
+		stbi_image_free(data);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		return texture;
+	}
 private:
 	class Charactor {
 	private:
 		GLFWwindow* window{ nullptr };
 		HWND hwnd{ nullptr };
+
+		const enum class CharactorState {
+			IDLE,
+			GRAB,
+			DROP,
+			COUNT
+		};
+
+		unsigned int texture[USHORT(CharactorState::COUNT)][10]{ NULL };
+		bool animation_end = false;
+
+		void CheckAnimationEnd(const short* num) {
+
+		}
 	public:
 		void Render() {
 
 		}
+
+		Charactor() {
+
+		}
+		~Charactor() {
+
+		}
 	};
+	// ----------------------------------------------------------
 	class Menu {
 	private:
 		GLFWwindow* window{ nullptr };
@@ -45,6 +95,13 @@ private:
 			return run;
 		}
 		void Render() {
+
+		}
+
+		Menu() {
+
+		}
+		~Menu() {
 
 		}
 	};
